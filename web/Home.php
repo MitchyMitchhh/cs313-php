c3ce24746a80f6b570633ed201b02cb726a5e8d9
Mitchell Porter <32944840+MitchyMitchhh@users.noreply.github.com>
2019-01-21 17:29:45 -0700

    adding more files

--- web/Home.php
+++ web/Home.php
@@ -1,11 +1,30 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<link rel="stylesheet" type="text/css" href="home.css">
-	<title>Home Page</title>
+	<title>Intro Page</title>
+
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	<link rel="stylesheet" href="main.css">
 
 </head>
-<body>
-	<p>Hello World</p>
+
+<a href="Assign.php">assign01</a></li>
+
+<body class="mountain-bg">
+
+<div class="container">
+
+<?php
+
+?>
+
+	<div class="jumbotron">
+	<h2>About Me</h2>
+
+	<p>At Make your own Mountain, we believe that everyone should have their own mountain. Whether you are ready for a full-scale mountain, or just getting started with your first hill in the backyard, we have you covered.</p>
+
+	</div>
+</div>
 </body>
 </html>
