b37fa9839ba7583a26024562298ec09209885fb1
Mitchell Porter <32944840+MitchyMitchhh@users.noreply.github.com>
2019-01-21 18:09:05 -0700

    image style

--- web/Home.php
+++ web/Home.php
@@ -3,7 +3,9 @@
 <head>
 	<title>Intro Page</title>
 
-	<link rel="stylesheet" href="main.css">
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	<link rel="stylesheet" href="home.css">
 
 </head>
 
