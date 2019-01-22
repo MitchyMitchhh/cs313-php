177626d496b10c0ad832944225cbc6ac9f26cf49
Mitchell Porter <32944840+MitchyMitchhh@users.noreply.github.com>
2019-01-21 18:02:26 -0700

    image style

--- web/Home.php
+++ web/Home.php
@@ -23,7 +23,7 @@
 	<p>Hello, my name is Mitchell Porter. I'm a 21-year-old who is a junior at BYUI, majoring in software engineering and minoring in data science. I love programming, but I don't have much of an artistic eye for design. My hobbies are mostly made of basketball, video games and going outside, but I'll do just about anything with good company, I like to try to diversify my interests when an opportunity arises.</p>
 
 
-	<img class="image-center" src="Friends.jpeg" alt="friends">
+	<img class="center" src="Friends.jpeg" alt="friends">
 
 </div>
 
