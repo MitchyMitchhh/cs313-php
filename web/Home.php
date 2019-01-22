07890f70bd75367a346354fce72aa02e05d560a1
Mitchell Porter <32944840+MitchyMitchhh@users.noreply.github.com>
2019-01-21 17:57:45 -0700

    image style

--- web/Home.php
+++ web/Home.php
@@ -17,13 +17,15 @@
 
 ?>
 
-	<div class="jumbotron">
+<div class="jumbotron">
 	<h2>About Me</h2>
 
-	<p>Hello, my name is Mitchell Porter. I'm a 21-year-old who is a junior at BYUI, majoring in software engineering and minoring in data science. I love programming, but I don't have much of an artistic eye for design. My hobbies are mostly made of basketball and video games, but I'll do just about anything with good company, I like to try to diversify my interests when an opportunity arises.</p>
+	<p>Hello, my name is Mitchell Porter. I'm a 21-year-old who is a junior at BYUI, majoring in software engineering and minoring in data science. I love programming, but I don't have much of an artistic eye for design. My hobbies are mostly made of basketball, video games and going outside, but I'll do just about anything with good company, I like to try to diversify my interests when an opportunity arises.</p>
 
-	<img src="Friends.jpeg" alt="friends">
+	<div class="image-center">
+		<img src="Friends.jpeg" alt="friends">
 	</div>
-	
+</div>
+
 </body>
 </html>
