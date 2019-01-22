2c022fd58734fae516298c8c9636094d5890b7df
Mitchell Porter <32944840+MitchyMitchhh@users.noreply.github.com>
2019-01-22 11:01:13 -0700

    php

---
+++ web/TA/Results.php
@@ -0,0 +1,16 @@
+<<?php session_start(); ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<<?php 
+	echo "Name: " . $_POST['name'];
+	echo "Email" ;
+	echo "Major" ;
+	echo "Comments" ;
+ ?>
+</body>
+</html>
