a070a02088522f23b5ed997149f7ab481f79a314
Mitchell Porter <32944840+MitchyMitchhh@users.noreply.github.com>
2019-01-27 00:33:05 -0700

    images

--- web/Browse.php
+++ web/Browse.php
@@ -13,15 +13,15 @@
 			<img class="resize" src="watermelon.jpg">
 			<img class="resize" src="banana.jpg">
 			<img class="resize" src="orange.jpg">
-			Which fruit do you want?<br />
+			<br />
+			<p>Which fruit do you want?</p><br />
 			<input type="checkbox" name="fruit" id="straw" value="straw"><label>Strawberries</label><br />
 			<input type="checkbox" name="fruit" id="water" value="water"><label>Watermelon</label><br />
 			<input type="checkbox" name="fruit" id="banan" value="banan"><label>Bananas</label><br />
 			<input type="checkbox" name="fruit" id="orang" value="orang"><label>Oranges</label><br />
 			<br />
 
-			<label for="comments">Comments:</label><br />
-			<textarea id="comments" name="comments" rows="4" cols="50"></textarea>
+
 			<br />
 			<input type="submit" value="Submit Answers">
 
