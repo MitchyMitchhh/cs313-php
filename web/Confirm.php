7eb5d0a23f41931fcce5d48ac25851b8f86b5cd8
Mitchell Porter <32944840+MitchyMitchhh@users.noreply.github.com>
2019-01-27 18:53:19 -0700

    sessions

---
+++ web/Confirm.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Confirmation Page</title>
+</head>
+
+<body>
+<p>Confirm your fruit order:</p>
+<?php
+foreach ($_SESSION[fruit] as $fruit)
+{
+	$fruit_clean = htmlspecialchars($fruit);
+	echo "<li><p>$fruit_clean</p></li>";
+}
+?>		
+
+</body>
+
+
+</html>
