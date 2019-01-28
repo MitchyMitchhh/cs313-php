ea684f5648fbc85815bc36a5ddbb3e745f19a1b5
Mitchell Porter <32944840+MitchyMitchhh@users.noreply.github.com>
2019-01-27 20:17:21 -0700

    removal

--- web/Cart.php
+++ web/Cart.php
@@ -31,10 +31,8 @@ foreach ($_SESSION[fruit] as $fruit)
 </form>
 
 <?
-$_SESSION["ufruit"] = $_POST["ufruit"];
-
-foreach ($_REQUEST[ufruit] as $key) {
-    unset($_SESSION[fruit][$key]);
+	foreach ($_REQUEST["ufruit"] as $key) {
+    	unset($_SESSION["fruit"][$key]);
 }
 ?>
 
