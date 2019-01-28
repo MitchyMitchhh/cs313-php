11c91a374b1546216a7e89dd77ec3cad58f6755f
Mitchell Porter <32944840+MitchyMitchhh@users.noreply.github.com>
2019-01-27 20:36:13 -0700

    removal

--- web/Cart.php
+++ web/Cart.php
@@ -17,29 +17,6 @@ foreach ($_SESSION[fruit] as $fruit)
 	echo "<li>$fruit_clean</li>";
 }
 ?>		
-<!-- 
-<p>Would you like to remove any of the items?</p>
-
- -->
-<!-- <form action="" post="POST">
-	<input type="checkbox" name="ufruit[]" id="straw" value="Strawberries"><label>Strawberries</label><br />
-	<input type="checkbox" name="ufruit[]" id="water" value="Watermelon"><label>Watermelon</label><br />
-	<input type="checkbox" name="ufruit[]" id="banan" value="Bananas"><label>Bananas</label><br />
-	<input type="checkbox" name="ufruit[]" id="orang" value="Oranges"><label>Oranges</label><br />
-	<br />
-	<input type="submit" value="Remove">
-</form> -->
-
-<?
-	// echo "<td><input type=checkbox name='remove[$key]' value=1>remove</td>";
-	
-	// foreach ($_REQUEST["remove"] as $key) {
- //    	unset($_SESSION["fruit"][$key]);
-	// }
-	// foreach ($_REQUEST["ufruit"] as $key) {
- //    	unset($_SESSION["fruit"][$key]);
-}
-?>
 
 <br>
 <a href="Browse.php">Go back to Browse!</a>
