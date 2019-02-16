35da7828cc972ab8ccd2156c7c7ee5b425d71e23
Mitchell Porter <32944840+MitchyMitchhh@users.noreply.github.com>
2019-02-15 19:35:12 -0700

    Please work database!

--- web/BackgroundAdd.php
+++ web/BackgroundAdd.php
@@ -16,45 +16,45 @@ $_SESSION["comment"] = $_POST["comment"];
 
 echo "$gamename $developer $publisher $releasedate $completeddate $completiontime $rating $reccomend $comment";
 
-require("dbConnect.php");
-$db = get_db();
-try
-{
-	// Add the Scripture
-	// We do this by preparing the query with placeholder values
-	$query = 'INSERT INTO public.game(gamename, developer, publisher, releasedate, datecompleted, completiontime) VALUES(:gamename, :developer, :publisher, :releasedate, :completeddate, :completiontime)';
+// require("dbConnect.php");
+// $db = get_db();
+// try
+// {
+// 	// Add the Scripture
+// 	// We do this by preparing the query with placeholder values
+// 	$query = 'INSERT INTO public.game(gamename, developer, publisher, releasedate, datecompleted, completiontime) VALUES(:gamename, :developer, :publisher, :releasedate, :completeddate, :completiontime)';
 	
-	$statement = $db->prepare($query);
-	// Now we bind the values to the placeholders. This does some nice things
-	// including sanitizing the input with regard to sql commands.
-	$statement->bindValue(':gamename', $gamename);
-	$statement->bindValue(':developer', $developer);
-	$statement->bindValue(':publisher', $publisher);
-	$statement->bindValue(':releasedate', $releasedate);
-	$statement->bindValue(':completeddate', $completeddate);
-	$statement->bindValue(':completiontime', $completiontime);
-	$statement->execute();
+// 	$statement = $db->prepare($query);
+// 	// Now we bind the values to the placeholders. This does some nice things
+// 	// including sanitizing the input with regard to sql commands.
+// 	$statement->bindValue(':gamename', $gamename);
+// 	$statement->bindValue(':developer', $developer);
+// 	$statement->bindValue(':publisher', $publisher);
+// 	$statement->bindValue(':releasedate', $releasedate);
+// 	$statement->bindValue(':completeddate', $completeddate);
+// 	$statement->bindValue(':completiontime', $completiontime);
+// 	$statement->execute();
 
-	$queryRating = 'INSERT INTO public.review(rating, reccomend, comment) VALUES(:rating, :reccomend, :comment)';
+// 	$queryRating = 'INSERT INTO public.review(rating, reccomend, comment) VALUES(:rating, :reccomend, :comment)';
 
-	$statement = $db->prepare($queryRating);
+// 	$statement = $db->prepare($queryRating);
 
-	$statement->bindValue(':rating', $rating);
-	$statement->bindValue(':reccomend', $reccomend);
-	$statement->bindValue(':comment', $comment);
-	$statement->execute();
-}
-catch (Exception $ex)
-{
-	// Please be aware that you don't want to output the Exception message in
-	// a production environment
-	echo "Error with DB. Details: $ex";
-	die();
-}
-// finally, redirect them to a new page to actually show the topics
-header("Location: AddGame.php");
-die(); // we always include a die after redirects. In this case, there would be no
-       // harm if the user got the rest of the page, because there is nothing else
-       // but in general, there could be things after here that we don't want them
-       // to see.
+// 	$statement->bindValue(':rating', $rating);
+// 	$statement->bindValue(':reccomend', $reccomend);
+// 	$statement->bindValue(':comment', $comment);
+// 	$statement->execute();
+// }
+// catch (Exception $ex)
+// {
+// 	// Please be aware that you don't want to output the Exception message in
+// 	// a production environment
+// 	echo "Error with DB. Details: $ex";
+// 	die();
+// }
+// // finally, redirect them to a new page to actually show the topics
+// header("Location: AddGame.php");
+// die(); // we always include a die after redirects. In this case, there would be no
+//        // harm if the user got the rest of the page, because there is nothing else
+//        // but in general, there could be things after here that we don't want them
+//        // to see.
 ?>
