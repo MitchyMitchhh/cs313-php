8adb848b3320a1cd0d339ef141770ab73ab7726f
Mitchell Porter <32944840+MitchyMitchhh@users.noreply.github.com>
2019-02-23 19:00:39 -0700

    add

--- web/GenerateUser.php
+++ web/GenerateUser.php
@@ -1,8 +1,21 @@
 <?php 
 
-	$pass = $_POST["password"];
+	session_start(); 
+	require('dbConnect.php');
 
-	$passwordHash = password_hash($pass, PASSWORD_DEFAULT);
+	$passwordHash = password_hash($_POST["password"], PASSWORD_DEFAULT);
 
+	$db = connectPostgres(); 
+	$sql = "INSERT INTO user(username, password) VALUES(:username, '" . $passwordHash . "')"; 
+
+	$query = $db->prepare($sql);
+	$query->bindValue(':username', $_POST["username"], PDO::PARAM_STR); 
+	$query->bindValue(':credits', $_POST["credits"], PDO::PARAM_INT); 
+	
+	$query->execute(); 
+	
+	header('Location: Login.php');
+	
+	die(); 
 
  ?>
