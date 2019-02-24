9974f609154fbf99926745545b810577548a844c
Mitchell Porter <32944840+MitchyMitchhh@users.noreply.github.com>
2019-02-23 19:58:49 -0700

    add

--- web/GenerateUser.php
+++ web/GenerateUser.php
@@ -6,10 +6,14 @@
 	$passwordHash = password_hash($_POST["password"], PASSWORD_DEFAULT);
 
 	$db = get_db(); 
-	$sql = "INSERT INTO public.user(loginname, password) VALUES(:loginname, '$passwordHash')"; 
+	$sql = "INSERT INTO public.user(firstname, lastname, email, loginname, password) VALUES(:firstname, :lastname, :email, :loginname, '$passwordHash')"; 
 
 	$query = $db->prepare($sql);
+	$query->bindValue(':firstname', $_POST["firstname"], PDO::PARAM_STR); 
+	$query->bindValue(':lastname', $_POST["lastname"], PDO::PARAM_STR); 
+	$query->bindValue(':email', $_POST["email"], PDO::PARAM_STR); 
 	$query->bindValue(':loginname', $_POST["loginname"], PDO::PARAM_STR); 
+	$query->bindValue(':password', $_POST["password"], PDO::PARAM_STR); 
 	
 	$query->execute(); 
 	
