992a158b14cf4e233ff245438c2bd9aad2043e8c
Mitchell Porter <32944840+MitchyMitchhh@users.noreply.github.com>
2019-02-26 01:59:54 -0700

    added in game user id

--- web/ViewGame.php
+++ web/ViewGame.php
@@ -22,7 +22,7 @@
 		$userId = $_SESSION["userId"];
 
 		if ($userId) {
-			$sql = "SELECT public.game.gamename, public.game.developer, public.game.publisher, public.game.releasedate, public.game.datecompleted, public.game.completiontime FROM public.game WHERE public.game.userid='$userId'";
+			$sql = "SELECT public.game.gamename, public.game.developer, public.game.publisher, public.game.releasedate, public.game.datecompleted, public.game.completiontime, public.review.rating, public.review.reccomend, public.review.comment FROM public.game JOIN public.review on public.game.reviewId = public.review.id WHERE public.game.userid='$userId'";
 			$query = $db->prepare($sql);
 	    	$query->execute();
 
@@ -33,11 +33,7 @@
 
 		}
 		else{
-			$query = $db->prepare("SELECT gamename, developer, publisher, releasedate datecompleted completiontime FROM public.game");
-			$query->bindValue(':id', $userId, PDO::PARAM_STR); 
-	    	$query->execute();
-			$row = $query->fetch(PDO::FETCH_ASSOC);
-	    	$gamename = $row["gamename"];
+			echo "Error: Not logged in to a registered account!";
 		}
 		
 	?>
