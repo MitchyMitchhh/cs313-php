82cee9a289b5c428b87d85bd4adfd8ba40e0a799
Mitchell Porter <32944840+MitchyMitchhh@users.noreply.github.com>
2019-02-25 20:11:43 -0700

    password

--- web/Authenticate.php
+++ web/Authenticate.php
@@ -19,16 +19,16 @@ function login($loginname, $password) {
   
   $queryResults = $query->fetch(PDO::FETCH_ASSOC); 
 
-  echo "password", $password, "rpassword", $queryResults["password"];
+  //echo "password", $password, "rpassword", $queryResults["password"];
    
   if (password_verify($password, $queryResults["password"])) {
     $_SESSION["userId"] = $queryResults["id"];
     $_SESSION["loginName"] = $queryResults["loginname"]; 
-    echo "working";
+    //echo "working";
     header("Location: ViewGame.php");
     die(); 
   } else {
-    //header("Location: Login.php"); 
+    header("Location: Login.php"); 
     die(); 
   }
 } 
