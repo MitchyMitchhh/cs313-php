564862308b6aaa6286f52c5fd5b8f5727e88f23b
Mitchell Porter <32944840+MitchyMitchhh@users.noreply.github.com>
2019-02-26 02:35:58 -0700

    added in game user id

--- web/BackgroundAdd.php
+++ web/BackgroundAdd.php
@@ -34,17 +34,17 @@ $reviewId = $queryResults["id"];
 
 echo $reviewId; 
 
-$sqlGame = "INSERT INTO game(gamename, developer, publisher, releasedate, datecompleted, completiontime, userid, reviewid) VALUES(:gamename, :developer, :publisher, :releasedate, :datecompleted, :completiontime, $userId, '$reviewId')";
+// $sqlGame = "INSERT INTO game(gamename, developer, publisher, releasedate, datecompleted, completiontime, userid, reviewid) VALUES(:gamename, :developer, :publisher, :releasedate, :datecompleted, :completiontime, $userId, '$reviewId')";
 
-$queryGame = $db->prepare($sqlGame);
-$queryGame->bindValue(':gamename', $_POST["gamename"], PDO::PARAM_STR); 
-$queryGame->bindValue(':developer', $_POST["developer"], PDO::PARAM_INT);  
-$queryGame->bindValue(':publisher', $_POST["publisher"], PDO::PARAM_INT); 
-$queryGame->bindValue(':releasedate', $_POST["releasedate"], PDO::PARAM_INT); 
-$queryGame->bindValue(':datecompleted', $_POST["datecompleted"], PDO::PARAM_INT); 
-$queryGame->bindValue(':completiontime', $_POST["completiontime"], PDO::PARAM_INT);
+// $queryGame = $db->prepare($sqlGame);
+// $queryGame->bindValue(':gamename', $_POST["gamename"], PDO::PARAM_STR); 
+// $queryGame->bindValue(':developer', $_POST["developer"], PDO::PARAM_INT);  
+// $queryGame->bindValue(':publisher', $_POST["publisher"], PDO::PARAM_INT); 
+// $queryGame->bindValue(':releasedate', $_POST["releasedate"], PDO::PARAM_INT); 
+// $queryGame->bindValue(':datecompleted', $_POST["datecompleted"], PDO::PARAM_INT); 
+// $queryGame->bindValue(':completiontime', $_POST["completiontime"], PDO::PARAM_INT);
 
-$queryGame->execute(); 
+// $queryGame->execute(); 
 
 header('Location: AddGame.php');
 die(); 
