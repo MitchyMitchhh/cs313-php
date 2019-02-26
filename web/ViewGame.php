b393953d67bb2e480a8e5e8a8f863fb3b2bca826
Mitchell Porter <32944840+MitchyMitchhh@users.noreply.github.com>
2019-02-25 22:12:21 -0700

    db pull

--- web/ViewGame.php
+++ web/ViewGame.php
@@ -28,10 +28,10 @@
 
 
 	if ($userId) {
-		$sql = "SELECT gamename, developer, publisher, releasedate, datecompleted, completiontime FROM public.game";
-		$query = $db->prepare($sql);
-		$query->bindValue(':id', $userId, PDO::PARAM_STR); 
-    	$query->execute();
+		//$sql = "SELECT gamename, developer, publisher, releasedate, datecompleted, completiontime FROM public.game";
+		//$query = $db->prepare($sql);
+		//$query->bindValue(':id', $userId, PDO::PARAM_STR); 
+    	//$query->execute();
 	// Go through each result
 	// while ($row = $query->fetch(PDO::FETCH_ASSOC))
 	// {
