0941e2f76fedf15cb570eb99e36da5bf403486a5
Mitchell Porter <32944840+MitchyMitchhh@users.noreply.github.com>
2019-02-26 00:45:12 -0700

    added in game user id

--- web/BackgroundAdd.php
+++ web/BackgroundAdd.php
@@ -3,22 +3,12 @@
 require 'dbConnect.php';
 
 session_start();
-// $_SESSION["firstname"] = $_POST["firstname"];
-// $_SESSION["lastname"] = $_POST["lastname"];
-// $_SESSION["email"] = $_POST["email"];
-//$_SESSION["gamename"] = $_POST["gamename"];
-// $_SESSION["developer"] = $_POST["developer"];
-// $_SESSION["publisher"] = $_POST["publisher"];
-// $_SESSION["releasedate"] = $_POST["releasedate"];
-// $_SESSION["completeddate"] = $_POST["completeddate"];
-// $_SESSION["completiontime"] = $_POST["completiontime"];
-// $_SESSION["rating"] = $_POST["rating"];
-// $_SESSION["reccomend"] = $_POST["reccomend"];
-// $_SESSION["comment"] = $_POST["comment"];
+
+$userId = $_SESSION["userId"];
 
 $db = get_db();
 
-$sqlGame = "INSERT INTO game(gamename, developer, publisher, releasedate, datecompleted, completiontime) VALUES(:gamename, :developer, :publisher, :releasedate, :datecompleted, :completiontime)";
+$sqlGame = "INSERT INTO game(gamename, developer, publisher, releasedate, datecompleted, completiontime, userid) VALUES(:gamename, :developer, :publisher, :releasedate, :datecompleted, :completiontime, $userId)";
 
 $queryGame = $db->prepare($sqlGame);
 $queryGame->bindValue(':gamename', $_POST["gamename"], PDO::PARAM_STR); 
@@ -45,18 +35,3 @@ header('Location: AddGame.php');
 die(); 
 
 ?>
-
-<!-- 
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-<?
-//echo "$gamename $developer $publisher $releasedate $completeddate $completiontime $rating $reccomend $comment";
-?>
-</body>
-</html>
-
- -->
