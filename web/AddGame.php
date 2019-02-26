720043c7c33bed383f1d452c0beafe45aeb36e8c
Mitchell Porter <32944840+MitchyMitchhh@users.noreply.github.com>
2019-02-25 23:57:56 -0700

    db pull

--- web/AddGame.php
+++ web/AddGame.php
@@ -1,64 +1,74 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Add Games</title>
+	
+	<!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <!--Import Google Icon Font-->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <!--Import materialize.css-->
-    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+    <!-- <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/> -->
     <!--Let browser know website is optimized for mobile-->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"/> -->
+
+	<title>Add Games</title>
+
 </head>
 
 <body>
-<p>Log your Video Game information:</p>
 
-<form method="POST" action="BackgroundAdd.php">
+	<div class="container"></div>
 
-	<label for="gamename">Game Name</label>
-	<input type="text" placeholder="Game Name" id="gamename" name="gamename">
-	<br />
+	<p>Log your Video Game information:</p>
 
-	<label for="developer">Developer</label>
-	<input type="text" placeholder="Developer" id="developer" name="developer">
-	<br />
+	<form method="POST" action="BackgroundAdd.php">
 
-	<label for="publisher">Publisher</label>
-	<input type="text" placeholder="Publisher" id="publisher" name="publisher">
-	<br />
+		<label for="gamename">Game Name</label>
+		<input type="text" placeholder="Game Name" id="gamename" name="gamename">
+		<br />
 
-	<label for="releasedate">Release Date</label>
-	<input type="date" placeholder="Release Date" id="releasedate" name="releasedate">
-	<br />
+		<label for="developer">Developer</label>
+		<input type="text" placeholder="Developer" id="developer" name="developer">
+		<br />
 
-	<label for="datecompleted">Completed Date</label>
-	<input type="date" placeholder="Completed Date" id="datecompleted" name="datecompleted">
-	<br />
+		<label for="publisher">Publisher</label>
+		<input type="text" placeholder="Publisher" id="publisher" name="publisher">
+		<br />
 
-	<label for="completiontime">Completion Time</label>
-	<input type="text" placeholder="Completion Time" id="completiontime" name="completiontime">
-	<br />
+		<label for="releasedate">Release Date</label>
+		<input type="date" placeholder="Release Date" id="releasedate" name="releasedate">
+		<br />
 
-	<label for="rating">Personal Rating</label>
-	<input type="number" placeholder="Rating" id="rating" name="rating" min="1" max="10">
-	<br />
+		<label for="datecompleted">Completed Date</label>
+		<input type="date" placeholder="Completed Date" id="datecompleted" name="datecompleted">
+		<br />
 
-	<label for="reccomend ">Reccomend Y/N</label>
-	<input type="radio" placeholder="Rec" id="reccomend" name="reccomend" value="Yes"> Yes
-	<input type="radio" placeholder="Rec" id="reccomend" name="reccomend" value="No"> No
-	<br />
+		<label for="completiontime">Completion Time</label>
+		<input type="text" placeholder="Completion Time" id="completiontime" name="completiontime">
+		<br />
 
-	<label for="comment">Extra Comment</label>
+		<label for="rating">Personal Rating</label>
+		<input type="number" placeholder="Rating" id="rating" name="rating" min="1" max="10">
+		<br />
+
+		<label for="reccomend ">Reccomend Y/N</label>
+		<input type="radio" placeholder="Rec" id="reccomend" name="reccomend" value="Yes"> Yes
+		<input type="radio" placeholder="Rec" id="reccomend" name="reccomend" value="No"> No
+		<br />
+
+		<label for="comment">Extra Comment</label>
+		<br>
+		<textarea cols="40" rows="5" id="comment" name="comment"></textarea>
+		<br />
+
+		<input type="submit" value="Submit">
+
+	</form>
+
+	<a href="ViewGame.php">View your game list</a>
 	<br>
-	<textarea cols="40" rows="5" id="comment" name="comment"></textarea>
-	<br />
 
-	<input type="submit" value="Submit">
-
-</form>
-
-<a href="ViewGame.php">View your game list</a>
-<br>
+	</div>
 
 </body>
 
