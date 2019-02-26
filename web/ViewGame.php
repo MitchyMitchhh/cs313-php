94a6281f5b67098de238ff868826ebd915ea40cf
Mitchell Porter <32944840+MitchyMitchhh@users.noreply.github.com>
2019-02-25 20:30:57 -0700

    password

--- web/ViewGame.php
+++ web/ViewGame.php
@@ -11,15 +11,28 @@ $db = get_db();
 <body>
 <div>
 
-<h1>Game Log</h1>
+<h1>View Game Log:</h1>
 
 <?php
 
-$statement = $db->prepare("SELECT gamename, developer, publisher, releasedate datecompleted completiontime FROM public.game");
-$statement->execute();
-// Go through each result
-while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-{
+  $userId = $_SESSION["userId"];
+  
+    $query = $db->prepare("SELECT credits
+                           FROM users
+                           WHERE id = :id"); 
+    //$query->bindValue(':id', $userId, PDO::PARAM_STR); 
+    //$query->execute(); 
+  
+    //$row = $query->fetch(PDO::FETCH_ASSOC);
+    //$credits = $row["credits"];
+	
+	if ($userId) {
+	$query = $db->prepare("SELECT gamename, developer, publisher, releasedate datecompleted completiontime FROM public.game WHERE id = :id");
+	$query->bindValue(':id', $userId, PDO::PARAM_STR); 
+    $query->execute();
+	// Go through each result
+	while ($row = $query->fetch(PDO::FETCH_ASSOC))
+	{
 	// The variable "row" now holds the complete record for that
 	// row, and we can access the different values based on their
 	// name
@@ -29,9 +42,10 @@ while ($row = $statement->fetch(PDO::FETCH_ASSOC))
 	$releasedate = $row['releasedate'];
 	$datecompleted = $datecompleted['datecompleted'];
 	$completiontime = $row['completiontime'];
+	}
 
 	echo "<p><strong>$gamename $developer $publisher $releasedate $datecompleted $completiontime</strong> - \"$content\"<p>";
-}
+	}
 ?>
 
 
