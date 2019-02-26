ccc60c08dffa4a087435047bf6924aefb0dfe0f4
Mitchell Porter <32944840+MitchyMitchhh@users.noreply.github.com>
2019-02-26 01:52:08 -0700

    added in game user id

--- web/ViewGame.php
+++ web/ViewGame.php
@@ -21,48 +21,10 @@
 		$db = get_db();
 		$userId = $_SESSION["userId"];
 
-	    //$query->bindValue(':id', $userId, PDO::PARAM_STR); 
-	    //$query->execute(); 
-	  
-	    //$row = $query->fetch(PDO::FETCH_ASSOC);
-	    //$credits = $row["credits"];
-		if ($userId) {
-			echo "hey";
-		}
-
-
 		if ($userId) {
 			$sql = "SELECT public.game.gamename, public.game.developer, public.game.publisher, public.game.releasedate, public.game.datecompleted, public.game.completiontime FROM public.game WHERE public.game.userid='$userId'";
 			$query = $db->prepare($sql);
-			//$query->bindValue(':id', $userId, PDO::PARAM_STR); 
 	    	$query->execute();
-		// Go through each result
-		// while ($row = $query->fetch(PDO::FETCH_ASSOC))
-		// {
-		// // The variable "row" now holds the complete record for that
-		// // row, and we can access the different values based on their
-		// // name
-		// $gamename = $row['gamename'];
-		// $developer = $row['developer'];
-		// $publisher = $row['publisher'];
-		// $releasedate = $row['releasedate'];
-		// $datecompleted = $datecompleted['datecompleted'];
-		// $completiontime = $row['completiontime'];
-		// }
-	 //    	$rows = $query->fetchAll();
-		
-		// 	foreach($rows as $row)
-	 //    	{
-		// 		$html_chunk = "<b>" . $row["gamename"] . " </b>" .
-	 //                    "<b>" . $row["developer"] . "</b>" . 
-	 //                    "<b>" . $row["publisher"] . "</b>" . 
-	 //                    "<b>" . $row["releasedate"] . "</b>" .
-	 //                    "<b>" . $row["datecompleted"] . "</b>" .
-	 //                    "\"" . $row["completiontime"] . "\" </br>";
-	    
-	 //    		echo $html_chunk;
-		// 	}
-		// //echo "<p><strong>$gamename $developer $publisher $releasedate $datecompleted $completiontime</strong> - \"$content\"<p>";
 
 	    	while ($row = $query->fetch(PDO::FETCH_ASSOC))
 			{
@@ -78,7 +40,6 @@
 	    	$gamename = $row["gamename"];
 		}
 		
-
 	?>
 
 	<a href="AddGame.php">Add More Games!</a>
