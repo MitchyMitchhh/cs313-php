7d9de36c6bc034466f1cd6ff97df74cc7bdf7df7
Mitchell Porter <32944840+MitchyMitchhh@users.noreply.github.com>
2019-02-25 21:41:55 -0700

    db pull

--- web/ViewGame.php
+++ web/ViewGame.php
@@ -23,38 +23,44 @@
     //$row = $query->fetch(PDO::FETCH_ASSOC);
     //$credits = $row["credits"];
 	
-	//if ($userId) {
+	if ($userId) {
 		$query = $db->prepare("SELECT gamename, developer, publisher, releasedate datecompleted completiontime FROM public.game");
 		$query->bindValue(':id', $userId, PDO::PARAM_STR); 
     	$query->execute();
 	// Go through each result
-	while ($row = $query->fetch(PDO::FETCH_ASSOC))
-	{
-	// The variable "row" now holds the complete record for that
-	// row, and we can access the different values based on their
-	// name
-	$gamename = $row['gamename'];
-	$developer = $row['developer'];
-	$publisher = $row['publisher'];
-	$releasedate = $row['releasedate'];
-	$datecompleted = $datecompleted['datecompleted'];
-	$completiontime = $row['completiontime'];
-	}
-  //   	$rows = $query->fetchAll();
+	// while ($row = $query->fetch(PDO::FETCH_ASSOC))
+	// {
+	// // The variable "row" now holds the complete record for that
+	// // row, and we can access the different values based on their
+	// // name
+	// $gamename = $row['gamename'];
+	// $developer = $row['developer'];
+	// $publisher = $row['publisher'];
+	// $releasedate = $row['releasedate'];
+	// $datecompleted = $datecompleted['datecompleted'];
+	// $completiontime = $row['completiontime'];
+	// }
+ //    	$rows = $query->fetchAll();
 	
-		// foreach($rows as $row)
-  //   	{
-		// 	$html_chunk = "<b>" . $row["gamename"] . " </b>" .
-  //                   "<b>" . $row["developer"] . "</b>" . 
-  //                   "<b>" . $row["publisher"] . "</b>" . 
-  //                   "<b>" . $row["releasedate"] . "</b>" .
-  //                   "<b>" . $row["datecompleted"] . "</b>" .
-  //                   "\"" . $row["completiontime"] . "\" </br>";
+	// 	foreach($rows as $row)
+ //    	{
+	// 		$html_chunk = "<b>" . $row["gamename"] . " </b>" .
+ //                    "<b>" . $row["developer"] . "</b>" . 
+ //                    "<b>" . $row["publisher"] . "</b>" . 
+ //                    "<b>" . $row["releasedate"] . "</b>" .
+ //                    "<b>" . $row["datecompleted"] . "</b>" .
+ //                    "\"" . $row["completiontime"] . "\" </br>";
     
-  //   		echo $html_chunk;
-		// }
-	echo "<p><strong>$gamename $developer $publisher $releasedate $datecompleted $completiontime</strong> - \"$content\"<p>";
-	//}
+ //    		echo $html_chunk;
+	// 	}
+	// //echo "<p><strong>$gamename $developer $publisher $releasedate $datecompleted $completiontime</strong> - \"$content\"<p>";
+
+    	while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+		{
+  			echo ' Game Name: ' . $row['gamename'] . ' Developer: ' . $row['developer'] . ' Publisher: ' . $row['publisher'] . ' Release Date: ' . $row['releasedate'] . ' Date Completed: ' . $row['datecompleted'] . ' Time Taken to Complete: ' . $row['completiontime'] . '<br/>';
+		}
+
+	}
 ?>
 
 <a href="AddGame.php">Add More Games!</a>
